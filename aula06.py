803f5c5729718420d425626aec53d00c17de9cc5
bruno bettiolo de oliveira <bruno.bettiolo.oliveira@escola.pr.gov.br>
2025-09-03 13:55:28 +0000

    aula 6 correlação

---
+++ aula06.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import numpy as np 
+
+# Criação de um datest fictício 
+np.random.seed (42)
+
+dados = pd.DataFrame({
+    "Orçamento Campanha": np.random.randint(1000, 5000, size=100)
+})
