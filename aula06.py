90bbbfd2540823fbdfb503ddfeb2c2a87ac9b52b
bruno bettiolo de oliveira <bruno.bettiolo.oliveira@escola.pr.gov.br>
2025-09-03 19:32:37 +0000

    aula 06 correlação parte 2

--- aula06.py
+++ aula06.py
@@ -1,9 +1,15 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 
-# Criação de um datest fictício 
-np.random.seed (42)
+# Criação de um dataset fictício
+np.random.seed(42)
 
 dados = pd.DataFrame({
-    "Orçamento Campanha": np.random.randint(1000, 5000, size=100)
+    "Orcamento Campanha": np.random.randint(2000, 5000, size=100),
+    "Visualizacoes Anuncio": np.random.randint(2000, 20000, size=100),
+    "Vendas": np.random.randint(10, 5000, size=100),
+    "Cliques": np.random.randint(100, 20000, size=100)
 })
+
+# Visualizar a correlação entre cada variável
+print(dados.corr().round(4))
